b2db3c3e2bef228f8566757953c8916fb801efde
usamapal01 <98360094+usamapal01@users.noreply.github.com>
2023-06-11 13:43:19 -0700

    Optimized code

--- Object3D_mat.py
+++ Object3D_mat.py
@@ -12,27 +12,53 @@ import numpy
 
 class Object3D:
     def __init__(self, mesh: Mesh3D,
-                 position: numpy.array = numpy.array([0.0, 0.0, 0.0]),
-                 orientation: numpy.array = numpy.array([0.0, 0.0, 0.0]),
-                 scale: numpy.array = numpy.array([0.5, 0.5, 0.5])):
+                 position: pygame.Vector3 = pygame.Vector3(0.0, 0.0, 0.0),
+                 orientation: pygame.Vector3 = pygame.Vector3(0.0, 0.0, 0.0),
+                 scale: pygame.Vector3 = pygame.Vector3(1.0, 1.0, 1.0),):
         self.mesh = mesh
         self.position = position
         self.orientation = orientation
         self.scale = scale
+        self.model_matrix = None
+        self.view_matrix = None
+        self.projection_matrix = None
 
-    def local_to_world(self, local_vertex: numpy.array) -> numpy.array:
-        """
-        Transforms the given local-space vertex to world space,
-        by applying the translation, orientation, and scale vectors
-        of the Object3D.
-        """
+    def projection_matrix_update(self, frustum):
+        near, far, left, right, bottom, top = frustum
 
-        # Finish this function to return a transformed world-space vertex.
-        # Remember the correct order of compound transformations:
-        # scale
-        # then rotate yaw, rotate pitch, rotate roll
-        # then translate.
+        self.projection_matrix = numpy.array([
+            [2 * near / (right - left), 0, (right + left) / (right - left), 0],
+            [0, 2 * near / (top - bottom), (top + bottom) / (top - bottom), 0],
+            [0, 0, (far + near) / (near - far), 2 * far * near / (near - far)],
+            [0, 0, -1, 0]
+        ])
 
+    def view_matrix_update(self, camera):
+        eyeX, eyeY, eyeZ, atX, atY, atZ, upX, upY, upZ = camera
+
+        forward = numpy.array([atX - eyeX, atY - eyeY, atZ - eyeZ])
+        forward = forward / numpy.linalg.norm(forward)
+
+        right = numpy.cross(forward, numpy.array([upX, upY, upZ]))
+        right = right / numpy.linalg.norm(right)
+
+        up = numpy.cross(right, forward)
+        up = up / numpy.linalg.norm(up)
+
+        eye = numpy.array([eyeX, eyeY, eyeZ])
+
+        tx = numpy.dot(eye, right)
+        ty = numpy.dot(eye, up)
+        tz = numpy.dot(eye, forward)
+
+        a_camera = numpy.array([[right[0], up[0], forward[0], 0],
+                                [right[1], up[1], forward[1], 0],
+                                [right[2], up[2], forward[2], 0],
+                                [tx, ty, tz, 1]])
+
+        self.view_matrix = numpy.linalg.inv(a_camera)
+
+    def model_matrix_update(self):
         # scale
         scale_mat = numpy.array([[self.scale[0], 0, 0, 0],
                                  [0, self.scale[1], 0, 0],
@@ -83,12 +109,32 @@ class Object3D:
         ])
 
         # rotate (multiplying matrices in correct order)
-        local_vertex = numpy.matmul(translated_mat, numpy.matmul(roll_mat, numpy.matmul(
-            pitch_mat, numpy.matmul(yaw_mat, numpy.matmul(scale_mat, local_vertex)))))
+        self.model_matrix = numpy.matmul(translated_mat, numpy.matmul(roll_mat, numpy.matmul(
+            pitch_mat, numpy.matmul(yaw_mat, scale_mat))))
 
-        return local_vertex
+        # # rotate
+        # self.model_matrix = numpy.matmul(translated_matrix, numpy.matmul(a_roll, numpy.matmul(
+        #     a_pitch, numpy.matmul(a_yaw, scale_matrix))))
 
-    def world_to_view(self, world_vertex, camera) -> numpy.array:
+    def local_to_world(self, local_vertex: numpy.array, model_matrix) -> numpy.array:
+        """
+        Transforms the given local-space vertex to world space,
+        by applying the translation, orientation, and scale vectors
+        of the Object3D.
+        """
+
+        # Finish this function to return a transformed world-space vertex.
+        # Remember the correct order of compound transformations:
+        # scale
+        # then rotate yaw, rotate pitch, rotate roll
+        # then translate.
+
+        # local_vertex = numpy.matmul(model_matrix, local_vertex)
+        # return local_vertex
+
+        return numpy.matmul(model_matrix, local_vertex)
+
+    def world_to_view(self, world_vertex, view_matrix) -> numpy.array:
         """
         Transforms the given world-space vertex to view space,
         by translating and rotating the object according to the
@@ -97,59 +143,24 @@ class Object3D:
 
         # We don't have a movable camera for this demo, so world space
         # is identical to view space.
-        eyeX, eyeY, eyeZ, atX, atY, atZ, upX, upY, upZ = camera
 
-        forward = numpy.array([atX - eyeX, atY - eyeY, atZ - eyeZ])
-        forward = forward / numpy.linalg.norm(forward)
+        # world_vertex = numpy.matmul(view_matrix, world_vertex)
+        # return world_vertex
 
-        right = numpy.cross(forward, numpy.array([upX, upY, upZ]))
-        right = right / numpy.linalg.norm(right)
+        return numpy.matmul(view_matrix, world_vertex)
 
-        up = numpy.cross(right, forward)
-        up = up / numpy.linalg.norm(up)
-
-        eye = numpy.array([eyeX, eyeY, eyeZ])
-
-        tx = numpy.dot(eye, right)
-        ty = numpy.dot(eye, up)
-        tz = numpy.dot(eye, forward)
-
-        a_camera = numpy.array([[right[0], up[0], forward[0], 0],
-                                [right[1], up[1], forward[1], 0],
-                                [right[2], up[2], forward[2], 0],
-                                [tx, ty, tz, 1]])
-
-        a_camera_inverse = numpy.linalg.inv(a_camera)
-
-        world_vertex = numpy.matmul(a_camera_inverse, world_vertex)
-
-        # view_vertex = numpy.matmul(a_camera_inverse, world_vertex)
-        # return view_vertex
-
-        return world_vertex
-
-    def view_to_clip(self, view_vertex, frustum) -> numpy.array:
+    def view_to_clip(self, view_vertex, projection_matrix) -> numpy.array:
         """
         Projects the view-space vertex to clip space(normalized device coordinates).
         """
         # Finish this function to compute (xn, yn, zn) by first projecting
         # to the near-plane coordinates (xp, yp, zp), and then interpolating
         # along the clip space 2x2x2 cube.
-        near, far, left, right, bottom, top = frustum
 
-        a_persp = numpy.array([
-            [2 * near / (right - left), 0, (right + left) / (right - left), 0],
-            [0, 2 * near / (top - bottom), (top + bottom) / (top - bottom), 0],
-            [0, 0, (far + near) / (near - far), 2 * far * near / (near - far)],
-            [0, 0, -1, 0]
-        ])
-
-        view_vertex = numpy.matmul(a_persp, view_vertex)
+        view_vertex = numpy.matmul(projection_matrix, view_vertex)
 
         view_vertex = numpy.array([view_vertex[0] / view_vertex[3],
-                                   view_vertex[1] / view_vertex[3],
-                                   view_vertex[2], view_vertex[3]
-                                   ])
+                                   view_vertex[1] / view_vertex[3], view_vertex[2], view_vertex[3]])
         return view_vertex
 
     def clip_to_screen(self, clip_vertex, surface: pygame.Surface) -> tuple[int, int]:
@@ -170,12 +181,37 @@ class Object3D:
         # the the positive y-axis goes DOWN in Pygame, but UP in clip space.
         return int(xs), int(ys)
 
-    def draw(self, surface: pygame.Surface, frustum, camera):
+    # def draw(self, surface: pygame.Surface, frustum, camera):
+    #     projected = []
+    #     for v_local in self.mesh.vertices:
+    #         v_world = self.local_to_world(v_local)
+    #         v_view = self.world_to_view(v_world, camera)
+    #         v_clip = self.view_to_clip(v_view, frustum)
+    #         v_screen = self.clip_to_screen(v_clip, surface)
+    #         projected.append(v_screen)
+    #
+    #     for tri in self.mesh.faces:
+    #         a, b, c = (
+    #             projected[tri[0]],
+    #             projected[tri[1]],
+    #             projected[tri[2]],
+    #         )
+    #         primitives.draw_triangle(surface, a, b, c, tri[3])
+
+    def draw(self, surface: pygame.Surface, model_matrix, view_matrix, projection_matrix):
         projected = []
+        # self.model_matrix_update()
+        # self.view_matrix_update(camera)
+        # self.projection_matrix_update(frustum)
         for v_local in self.mesh.vertices:
-            v_world = self.local_to_world(v_local)
-            v_view = self.world_to_view(v_world, camera)
-            v_clip = self.view_to_clip(v_view, frustum)
+            # v_world = self.local_to_world(v_local)
+            # v_view = self.world_to_view(v_world)
+            # v_clip = self.view_to_clip(v_view, frustum)
+            # v_screen = self.clip_to_screen(v_clip, surface)
+            # projected.append(v_screen)
+            v_world = self.local_to_world(v_local, model_matrix)
+            v_view = self.world_to_view(v_world, view_matrix)
+            v_clip = self.view_to_clip(v_view, projection_matrix)
             v_screen = self.clip_to_screen(v_clip, surface)
             projected.append(v_screen)
 
--- softwaredrawing.py
+++ softwaredrawing.py
@@ -4,6 +4,7 @@ import math
 # from Object3D_mat import Object3D
 from Object3D_mat import Object3D
 
+
 ######
 # REQUIREMENTS:
 # make a file "primitives.py", and copy all your draw_line, draw_triangle, and fill_triangle code there.
@@ -21,7 +22,7 @@ if __name__ == "__main__":
     screen = pygame.display.set_mode((screen_width, screen_height))
     done = False
     m = make_cube()
-    m.position = pygame.Vector3(0, 0, -2)
+    m.position = pygame.Vector3(0, 0, -5)
 
     # Given the vertical half-FOV, compute coordinates of the perspective frustum.
     v_fov = 30
@@ -36,21 +37,51 @@ if __name__ == "__main__":
     print(near, far, left, right, bottom, top)
     frustum = (near, far, left, right, bottom, top)
     black = pygame.Color(0, 0, 0)
-    # m.scale[0] = 1
-    # m.scale[1] = 1
-    # m.scale[2] = 1
+    m.scale[0] = 0.25
+    m.scale[1] = 0.25
+    m.scale[2] = 0.25
+
+    # static_orientation = m.orientation
+    # static_position = m.position
+    # static_scale = m.scale
     camera = (1, 3, -2, 1, 3, -5, 0, 1, 0)
 
+    m.model_matrix_update()
+    m.view_matrix_update(camera)
+    m.projection_matrix_update(frustum)
+
+    curr_orientation = (m.orientation[0], m.orientation[1], m.orientation[2])
+    curr_scale = (m.scale[0], m.scale[1], m.scale[2])
+    curr_position = (m.position[0], m.position[1], m.position[2])
+    curr_camera = (camera[0], camera[1], camera[2], camera[3], camera[4], camera[5], camera[6], camera[7], camera[8])
+    curr_frustum = (frustum[0], frustum[1], frustum[2], frustum[3], frustum[4], frustum[5])
 
     while not done:
         screen.fill(black)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
+
+        # checking if the cube has been moved, scaled, or turned
+        if curr_orientation != m.orientation or curr_position != m.position or curr_scale != m.scale:
+            m.model_matrix_update()
+
+            curr_orientation = (m.orientation[0], m.orientation[1], m.orientation[2])
+            curr_scale = (m.scale[0], m.scale[1], m.scale[2])
+            curr_position = (m.position[0], m.position[1], m.position[2])
+        if curr_camera != (
+                camera[0], camera[1], camera[2], camera[3], camera[4], camera[5], camera[6], camera[7], camera[8]):
+            curr_camera = (
+                camera[0], camera[1], camera[2], camera[3], camera[4], camera[5], camera[6], camera[7], camera[8])
+            m.projection_matrix_update(curr_camera)
+        if curr_frustum != (frustum[0], frustum[1], frustum[2], frustum[3], frustum[4], frustum[5]):
+            curr_frustum = (frustum[0], frustum[1], frustum[2], frustum[3], frustum[4], frustum[5])
+            m.view_matrix_update(curr_frustum)
+
         m.orientation += pygame.Vector3(0.001, 0.001, 0.001)
         m.position += pygame.Vector3(0.00, 0.00, -0.001)  # backwards
         m.scale += pygame.Vector3(-0.001, -0.001, -0.001)  # reverted
 
-        m.draw(screen, frustum, camera)
+        m.draw(screen, m.model_matrix, m.view_matrix, m.projection_matrix)
         pygame.display.flip()
     pygame.quit()
